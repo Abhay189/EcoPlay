b45a7dd5b5034b1c0d5d1c0f965440287c6e411e
ss1106 <sukriti.sharma@ucalgary.ca>
2023-11-11 15:51:31 -0700

    fixed link

--- Assets/Scripts/PlayScript.cs
+++ Assets/Scripts/PlayScript.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManager;
+using UnityEngine.SceneManagement;
 
 public class PlayScript : MonoBehaviour
 {
     public void PlayGame()
+    {
         SceneManager.LoadScene("GameMenu");
+    }
 }
