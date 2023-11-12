ca64a6780341f281c888b791eaeb1881d60a303a
ss1106 <sukriti.sharma@ucalgary.ca>
2023-11-12 09:52:17 -0700

    tree and waste linked

---
+++ Assets/Scripts/GameOptions.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOptions : MonoBehaviour
+{
+    public void redirectToTreeGame() 
+    {
+        SceneManager.LoadScene("Tree Game");
+    }
+
+    public void redirectToWasteSorter() 
+    {
+        SceneManager.LoadScene("WasteSorter");
+    }
+}
